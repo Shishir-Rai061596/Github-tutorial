9f6fdd56a47e0a6fbe2489cefeaa392d450a8d64
Shishir-Rai061596 <shishir.rai.m.j@gmail.com>
2022-11-15 15:56:34 +0530

    Revert "Added new Component"
    This reverts commit 7799a9c654d3110b2a34c9bfdc4bd5022a1dd09e.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-import NewComponent from "./Components/NewComponent";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <NewComponent />
     </div>
   );
 }
--- src/Components/NewComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewComponent = () => {
-  return <div>NewComponent</div>;
-};
-
-export default NewComponent;
